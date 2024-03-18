7c2085f951618bbd4f5f81df54983ffe4095df71
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-03-18 19:27:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const arrayPalos = ["corazones", "picas", "treboles", "diamante"];
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(arrayPalos[Math.floor(Math.random() * 4)]);
 };
